11812640a7a693783691719da48549dbfa9236ff
Nathan Leiby <nathanleiby@gmail.com>
2021-08-13 18:14:48 -0700

    continue building scanner

--- cmd/scanner.go
+++ cmd/scanner.go
@@ -10,7 +10,7 @@ type Token struct {
 }
 
 func (t Token) String() string {
-	return fmt.Sprintf("%s %s %s", string(t.tokenType), t.lexeme, t.literal)
+	return fmt.Sprintf("%03d %s %s", t.tokenType, t.lexeme, t.literal)
 }
 
 type Scanner struct {
@@ -22,30 +22,82 @@ type Scanner struct {
 	tokens []Token
 }
 
-func (s Scanner) Tokens() []Token {
+func (s *Scanner) Tokens() []Token {
 	return s.tokens
 }
 
-func (s Scanner) isAtEnd() bool {
+func (s *Scanner) isAtEnd() bool {
 	return s.current >= len(s.source)
 }
 
-func (s Scanner) advance() byte {
-	s.current++
+func (s *Scanner) advance() byte {
+	val := s.source[s.current]
+	s.current = s.current + 1
+	return val
+}
+
+func (s *Scanner) peek() byte {
+	if s.isAtEnd() {
+		return 0
+	}
 	return s.source[s.current]
 }
 
-func (s Scanner) addToken(tt TokenType) {
+// match is a "conditional advance()".
+// It checks if the current character matches, and advances if so/
+func (s *Scanner) match(expected byte) bool {
+	if s.isAtEnd() {
+		return false
+	}
+	if s.source[s.current] != expected {
+		return false
+	}
+	// TODO: Is this OK refactor?
+	// if s.peek() != expected {
+	// 	return false
+	// }
+
+	s.current = s.current + 1
+	return true
+}
+
+func (s *Scanner) scanStringToken() {
+	for s.peek() != '"' && !s.isAtEnd() {
+		if s.peek() == '\n' {
+			s.line++
+		}
+		s.advance()
+	}
+
+	if s.isAtEnd() {
+		loxError(s.line, "Unterminated string.")
+		return
+	}
+
+	s.advance() // Consume the closing "
+
+	// Trim the surrounding quotes.
+	// value := s.source[s.start+1 : s.current-1]
+	s.addToken(STRING) //  TODO: addTokenliteral with real value
+}
+
+func (s *Scanner) addToken(tt TokenType) {
 	text := s.source[s.start:s.current]
 	s.tokens = append(s.tokens, Token{
 		tokenType: tt,
 		lexeme:    text,
 		line:      s.line,
 	})
-
 }
 
-func (s Scanner) scanToken() {
+// // TODO: Support non-strings, like numbers/etc
+// // the original code has an Object in java. Maybe interface{} could work here?
+// func (s *Scanner) addTokenLiteral(tt TokenType, value string) {
+//     text = source.substring(start, current);
+//     tokens.add(new Token(type, text, literal, line));
+// }
+
+func (s *Scanner) scanToken() {
 	c := s.advance()
 	switch c {
 	case '(':
@@ -68,13 +120,52 @@ func (s Scanner) scanToken() {
 		s.addToken(SEMICOLON)
 	case '*':
 		s.addToken(STAR)
+	case '!':
+		if s.match('=') {
+			s.addToken(BANG_EQUAL)
+		} else {
+			s.addToken(BANG)
+		}
+	case '=':
+		if s.match('=') {
+			s.addToken(EQUAL_EQUAL)
+		} else {
+			s.addToken(EQUAL)
+		}
+	case '<':
+		if s.match('=') {
+			s.addToken(LESS_EQUAL)
+		} else {
+			s.addToken(LESS)
+		}
+	case '>':
+		if s.match('=') {
+			s.addToken(GREATER_EQUAL)
+		} else {
+			s.addToken(GREATER)
+		}
+	case '/':
+		if s.match('/') {
+			for s.peek() != '\n' && !s.isAtEnd() {
+				s.advance()
+			}
+		} else {
+			s.addToken(SLASH)
+		}
+	// Whitespace
+	case ' ', '\r', '\t':
+		// Ignore whitespace.
+	case '\n':
+		s.line = s.line + 1
+	case '"':
+		s.scanStringToken()
 	default:
 		loxError(s.line, fmt.Sprintf("unexpected character: %c", c))
 	}
 
 }
 
-func (s Scanner) ScanTokens() error {
+func (s *Scanner) ScanTokens() error {
 	s.start = 0
 	s.current = 0
 	s.line = 1
@@ -93,8 +184,8 @@ func (s Scanner) ScanTokens() error {
 	return nil
 }
 
-func NewScanner(source string) Scanner {
-	return Scanner{
+func NewScanner(source string) *Scanner {
+	return &Scanner{
 		source: source,
 	}
 }
